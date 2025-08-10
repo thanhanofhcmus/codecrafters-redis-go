e8bf7ca87d4149f600bb0146d800a58684a6e090
Nguyen Thanh An <thanhan030301@gmail.com>
2025-08-10 23:36:45 +0700

    add PONG message

--- app/main.go
+++ app/main.go
@@ -1,24 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
-	"os"
 )
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind", err)
-		os.Exit(0)
+		log.Fatalln("Failed to bind", err)
 	}
 
 	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Failed to accept conn", err)
-		os.Exit(0)
+		log.Fatalln("Failed to accept new connection", err)
 	}
 
-	_ = conn
-
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		log.Fatalln("Failed to PONG", err)
+	}
 }
