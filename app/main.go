c6b1f0b823e7d5574a42f42017bc1a7264c3a77c
Nguyen Thanh An <thanhan030301@gmail.com>
2025-08-10 23:44:20 +0700

    add handle multple client

--- app/main.go
+++ app/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"io"
 	"log"
 	"net"
 )
+
+func handleConn(conn net.Conn) {
+	bufReader := bufio.NewReader(conn)
+	for {
+		data, err := bufReader.ReadBytes('\n')
+		if err != nil && err != io.EOF {
+			log.Println("Failed to read data", err)
+			return
+		}
+
+		log.Println("READ: ", string(data))
+
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			log.Println("Failed to PONG", err)
+			return
+		}
+	}
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -11,13 +32,12 @@ func main() {
 		log.Fatalln("Failed to bind", err)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		log.Fatalln("Failed to accept new connection", err)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatalln("Failed to accept new connection", err)
+		}
 
-	_, err = conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		log.Fatalln("Failed to PONG", err)
+		go handleConn(conn)
 	}
 }
