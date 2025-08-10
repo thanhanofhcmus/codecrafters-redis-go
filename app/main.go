64e1d67ec5c5ec105b798d093e6b46b374f90dd0
Nguyen Thanh An <thanhan030301@gmail.com>
2025-08-10 23:30:53 +0700

    add tcp server

--- app/main.go
+++ app/main.go
@@ -6,24 +6,19 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind", err)
+		os.Exit(0)
+	}
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Failed to accept conn", err)
+		os.Exit(0)
+	}
+
+	_ = conn
+
 }
