a6d49902d034b0e7c264af8471dd12849689885d
Nguyen Thanh An <thanhan030301@gmail.com>
2025-08-11 21:52:17 +0700

    hack to handle new PING

--- app/main.go
+++ app/main.go
@@ -10,13 +10,24 @@ import (
 func handleConn(conn net.Conn) {
 	bufReader := bufio.NewReader(conn)
 	for {
-		data, err := bufReader.ReadBytes('\n')
-		if err != nil && err != io.EOF {
+		var data []byte
+		var err error
+
+		// a hack since a simple PING command changed from +PING to *1\r\n$4\r\nPING\r\n
+		data, err = bufReader.ReadBytes('\n')
+		log.Printf("READ 1: %d, `%s`, %s", len(data), string(data), err)
+		data, err = bufReader.ReadBytes('\n')
+		log.Printf("READ 2: %d, `%s`, %s", len(data), string(data), err)
+		data, err = bufReader.ReadBytes('\n')
+		log.Printf("READ 3: %d, `%s`, %s", len(data), string(data), err)
+
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
 			log.Println("Failed to read data", err)
 			return
 		}
-
-		log.Println("READ: ", string(data))
 
 		_, err = conn.Write([]byte("+PONG\r\n"))
 		if err != nil {
