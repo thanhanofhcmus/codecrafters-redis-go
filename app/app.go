9e4f7ad63a00a078fd50840e3798abde4093f18c
Nguyen Thanh An <thanhan030301@gmail.com>
2025-08-16 00:13:55 +0700

    and convertArgsCmdToString

--- app/app.go
+++ app/app.go
@@ -6,6 +6,27 @@ type App struct {
 	m map[string]string
 }
 
+func convertArgsCmdToString(cmd Cmd) ([]string, error) {
+	if cmd.Sym != SymArray {
+		return nil, fmt.Errorf("expect command type to be Array, got %c", cmd.Sym)
+	}
+	args := cmd.Array
+	if len(args) == 0 {
+		return nil, fmt.Errorf("command cannot have zero argument")
+	}
+	result := make([]string, 0, len(args))
+	for _, arg := range args {
+		if arg.Sym != SymBulkString {
+			return nil, fmt.Errorf("expect argument to be of the type BulkString, got %c", arg.Sym)
+		}
+		if len(arg.BulkString) != 0 {
+			return nil, fmt.Errorf("argument is empty")
+		}
+		result = append(result, arg.BulkString)
+	}
+	return result, nil
+}
+
 func NewApp() *App {
 	return &App{
 		m: map[string]string{},
@@ -13,36 +34,27 @@ func NewApp() *App {
 }
 
 func (app *App) HandleCommand(cmd Cmd) (Cmd, error) {
-	if cmd.Sym != SymArray {
-		return Cmd{}, fmt.Errorf("cannot handle command that is not of array type")
-	}
-	args := cmd.Array
-	if len(args) == 0 {
-		return Cmd{}, fmt.Errorf("command cannot have zero size")
-	}
-	fArg := args[0]
-	if fArg.Sym != SymBulkString {
-		return Cmd{}, fmt.Errorf("first argument is not a BulkString")
+	args, err := convertArgsCmdToString(cmd)
+	if err != nil {
+		return Cmd{}, err
 	}
 
 	var result Cmd
 
-	switch fArg.BulkString {
+	switch args[0] {
 	case "PING":
 		result.Sym = SymString
 		result.String = "PONG"
 	case "ECHO":
 		result.Sym = SymBulkString
 		if len(args) >= 2 {
-			result.BulkString = args[1].BulkString
+			result.BulkString = args[1]
 		}
 	case "SET":
 		if len(args) < 3 {
 			return Cmd{}, fmt.Errorf("invalid number of arguments")
 		}
-		// TODO: handle type is not bulk string
-		key, value := args[1], args[2]
-		app.m[key.BulkString] = value.BulkString
+		app.m[args[1]] = args[2]
 		result.Sym = SymString
 		result.String = "OK"
 	case "GET":
@@ -50,14 +62,14 @@ func (app *App) HandleCommand(cmd Cmd) (Cmd, error) {
 			return Cmd{}, fmt.Errorf("invalid number of arguments")
 		}
 		// TODO: handle type is not bulk string
-		if value, exists := app.m[args[1].BulkString]; exists {
+		if value, exists := app.m[args[1]]; exists {
 			result.Sym = SymBulkString
 			result.BulkString = value
 		} else {
 			result.Sym = SymNull
 		}
 	default:
-		return Cmd{}, fmt.Errorf("unknown command `%s`", fArg.BulkString)
+		return Cmd{}, fmt.Errorf("unknown command `%s`", args[0])
 	}
 
 	return result, nil
